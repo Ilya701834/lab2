e9e85f141e37a0f052dee64b638ad870d0020511
Ilya Gordynets <ilyagordynets@gmail.com>
2021-11-05 16:08:47 +0300

    feat: Creating the menu.router.js

---
+++ src/resources/menus/menu.router.js
@@ -0,0 +1,83 @@
+const { StatusCodes } = require('http-status-codes');
+const router = require('express').Router();
+const Menu = require('./menu.model');
+
+const menusService = require('./menu.service');
+const catchErrors = require('../../common/catchErrors');
+
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const menus = await menusService.getAll();
+
+    res.json(menus.map(Menu.toResponse));
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const {title, photo, isPublish} = req.body;
+
+    const menu = await menusService.createMenu({title, photo, isPublish});
+
+    if (menu) {
+      res.status(StatusCodes.CREATED).json(Menu.toResponse(menu));
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ code: 'MENU_NOT_CREATE', msg: 'Menu not create' });
+    }
+  })
+);
+
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const menu = await menusService.getById(id);
+
+    if (menu) {
+      res.json(Menu.toResponse(menu));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'MENU_NOT_FOUND', msg: 'Menu not found' });
+    }
+  })
+);
+
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+    const {title, photo, isPublish} = req.body;
+
+    const menu = await menusService.updateById({id, title, photo, isPublish});
+
+    if (menu) {
+      res.status(StatusCodes.OK).json(Menu.toResponse(menu));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'MENU_NOT_FOUND', msg: 'Menu not found' });
+    }
+  })
+);
+
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const menu = await menusService.deleteById(id);
+
+    if (!menu) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'MENU_NOT_FOUND', msg: 'Menu not found' });
+    }
+
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ code: 'MENU_DELETED', msg: 'The menu has been deleted' });
+  })
+);
+
+module.exports = router;
