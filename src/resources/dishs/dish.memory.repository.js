dc2cdeaafc9582336835b121fd5903ce156621ad
Ilya Gordynets <ilyagordynets@gmail.com>
2021-11-05 16:04:45 +0300

    feat: Creating the dish.memory.repository.js

---
+++ src/resources/dishs/dish.memory.repository.js
@@ -0,0 +1,45 @@
+const Dish = require('./dish.model')
+
+const Dishes = [new Dish()];
+
+const getAll = async () => Dishes;
+
+const getById = async (id) => Dishes.find((dish) => dish.id === id);
+
+const createDish = async ({title, description, photo, isPublish, ingredients, price}) => {
+  const dish = new Dish({ title, description, photo, isPublish, ingredients, price });
+  Dishes.push(dish);
+  return dish;
+};
+
+const deleteById = async (id) => {
+  const dishPosition = Dishes.findIndex((dish) => dish.id === id);
+
+  if (dishPosition === -1) return null;
+
+  const dishDeletable = Dishes[dishPosition];
+
+  Dishes.splice(dishPosition, 1);
+  return dishDeletable;
+};
+
+const updateById = async ({id, title, description, photo, isPublish, ingredients, price}) => {
+  const dishPosition = Dishes.findIndex((dish) => dish.id === id);
+
+  if (dishPosition === -1) return null;
+
+  const oldDish = Dishes[dishPosition];
+  const newDish = { ...oldDish, title, description, photo, isPublish, ingredients, price};
+
+  Dishes.splice(dishPosition, 1, newDish);
+  return newDish;
+};
+
+module.exports = {
+  Dishes,
+  getAll,
+  getById,
+  createDish,
+  deleteById,
+  updateById,
+};
