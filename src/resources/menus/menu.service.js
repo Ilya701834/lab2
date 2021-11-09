f10ddbe64a204ad7749402e00274ec8bac388e5f
Ilya Gordynets <ilyagordynets@gmail.com>
2021-11-09 16:00:33 +0300

    feat: adding linked delete for service

--- src/resources/menus/menu.service.js
+++ src/resources/menus/menu.service.js
@@ -1,4 +1,6 @@
 const menusRepo = require('./menu.memory.repository');
+const categoriesRepo = require('../categorys/categorys.memory.repository');
+const dishesRepo = require('../dishs/dish.memory.repository');
 
 
 const getAll = () => menusRepo.getAll();
@@ -8,9 +10,14 @@ const createMenu = ({ title, photo, isPublish }) =>
 const deleteById = async (id) => {
   const menuDeletable = await getById(id);
   menusRepo.deleteById(id)
+  categoriesRepo.deleteByMenuId(id)
+  dishesRepo.deleteByCategoryId(categoriesRepo.getCategoryIdByMenuId.id)
   return menuDeletable;
 };
 const updateById = ({ id, title, photo, isPublish }) =>
   menusRepo.updateById({ id, title, photo, isPublish });
 
-module.exports = { getAll, getById, createMenu, deleteById, updateById };
+const getCategoryIdByMenuId = (id) =>
+  categoriesRepo.getCategoryIdByMenuId(id)
+
+module.exports = { getAll, getById, createMenu, deleteById, updateById, getCategoryIdByMenuId};
