2e8443519ac92b532c272c6ea4b80c408fef049a
Ilya Gordynets <ilyagordynets@gmail.com>
2021-11-24 15:18:33 +0300

    feat: create uncaughtHandler

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import config from './common/config';
 import app from './app';
+import { uncaughtException, unhandledRejection } from './middleware'
 
 const { PORT } = config;
 
 app.listen(PORT, () =>
   process.stdout.write(`App is running on http://localhost:${PORT}`)
 );
+
+process.on('uncaughtException', uncaughtException());
+process.on('unhandledRejection', unhandledRejection());
