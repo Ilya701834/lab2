a8c23a27ef4458edceb13f9fddb6b89130eba9e7
Ilya Gordynets <ilyagordynets@gmail.com>
2021-11-24 15:17:52 +0300

    feat: create errorHandler

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import menuRouter from './resources/menus/menu.router';
 import dishRouter from './resources/dishs/dish.router';
 import categoryRouter from './resources/categorys/category.router';
+import { successHttpLogger, errorHttpLogger, errorHandler } from './middleware';
 
 const app = express();
 
@@ -16,8 +17,13 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use(successHttpLogger);
+app.use(errorHttpLogger);
+
 app.use('/menus', menuRouter);
 app.use('/dishes', dishRouter);
 app.use('/categories', categoryRouter);
+
+app.use(errorHandler)
 
 export default app;
