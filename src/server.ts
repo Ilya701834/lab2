70cad3700741903ba0a0c40108df9a32ff5234ef
Ilya Gordynets <ilyagordynets@gmail.com>
2021-12-10 16:47:24 +0300

    feat: bd connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import config from './common/config';
 import app from './app';
+import { connectedDB } from './db';
 import { uncaughtException, unhandledRejection } from './middleware'
 
 const { PORT } = config;
 
-app.listen(PORT, () =>
-  process.stdout.write(`App is running on http://localhost:${PORT}`)
-);
+const server = async () => {
 
-process.on('uncaughtException', uncaughtException());
-process.on('unhandledRejection', unhandledRejection());
+  await connectedDB();
+  app.listen(PORT, () =>
+    process.stdout.write(`App is running on http://localhost:${PORT}`)
+  );
+
+  process.on('uncaughtException', uncaughtException());
+  process.on('unhandledRejection', unhandledRejection());
+
+}
+
+server()
