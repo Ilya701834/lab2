74e66ffdba7ea83674080c43d1d45d3bbba984c4
Ilya Gordynets <ilyagordynets@gmail.com>
2021-12-15 14:40:51 +0300

    feat: add migration for users

---
+++ src/migration/1639311289412-users.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class users1639311289412 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+              name: 'users',
+              columns: [
+                  {
+                      name: 'id',
+                      generationStrategy: 'uuid',
+                      isGenerated: true,
+                      type: 'uuid',
+                      isPrimary: true,
+                  },
+                  {
+                      name: 'name',
+                      type: 'varChar',
+                  },
+                  {
+                      name: 'login',
+                      type: 'varChar',
+                  },
+                  {
+                      name: 'password',
+                      type: 'varChar',
+                  },
+              ],
+          }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
